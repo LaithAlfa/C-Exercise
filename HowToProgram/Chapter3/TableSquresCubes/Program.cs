12513badc062e9e6de200f478802f9c5f254e9ab
laithalfa <laith.alfaloujeh@gmail.com>
2020-12-25 16:03:02 -0600

    i think that there is a better sulotion then what i have here

---
+++ HowToProgram/Chapter3/TableSquresCubes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TableSquresCubes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //How to program Visual C# 3.29 Table of Squares and Cubes
+
+            Console.WriteLine("Number\tSquare\tCube");
+            Console.WriteLine(0+"\t"+0+"\t"+0 );
+            Console.WriteLine(1+"\t"+1*1+"\t"+1*1*1);
+            Console.WriteLine(2+"\t"+2*2+"\t"+2*2*2);
+            Console.WriteLine(3+"\t"+3*3+"\t"+3*3*3);
+            Console.WriteLine(4+"\t"+4*4+"\t"+4*4*4);
+            Console.WriteLine(5+"\t"+5*5+"\t"+5*5*5);
+            Console.WriteLine(6+"\t"+6*6+"\t"+6*6*6);
+            Console.WriteLine(7+"\t"+7*7+"\t"+7*7*7);
+            Console.WriteLine(8+"\t"+8*8+"\t"+8*8*8);
+            Console.WriteLine(9+"\t"+9*9+"\t"+9*9*9);
+            Console.WriteLine(10+"\t"+10*10+"\t"+10*10*10);
+
+        }
+    }
+}
